89126c0085cf0e581493ea5b69384d7a1d9683cf
Alex <pumpcin14@mail.ru>
2024-04-14 23:06:04 +0300

    Update build.gradle.kts

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -202,7 +202,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
                 "versionName" to "v$verName",
                 "versionCode" to verCode,
                 "authorList" to authors,
-                "updateJson" to "https://raw.githubusercontent.com/pumPCin_mod/LSPosed/main/${flavorLowered}.json",
+                "updateJson" to "https://raw.githubusercontent.com/pumPCin/LSPosed_mod/main/${flavorLowered}.json",
                 "requirement" to when (flavorLowered) {
                     "riru" -> "Requires Riru $moduleMinRiruVersionName or above installed"
                     "zygisk" -> "Requires Magisk 24.0+ and Zygisk enabled"
